10c167fc2b9a5837e9bda14d4e4df76326bc919e
Nathan Van der Auwera <nathan@dixis.com>
2013-03-22 14:01:19 +0100

    Remove ie6 support.
    Since jquery 1.9 no longer supports $.browser 
    and ie6 marketshare dead.
    Alternative fix: http://stackoverflow.com/questions/14512826/impromptu-with-jquery-1-9-error-with-browser-msie

--- jQuery.equalHeights.js
+++ jQuery.equalHeights.js
@@ -24,8 +24,6 @@ $.fn.equalHeights = function(px) {
 			if ($(this).outerHeight() > currentTallest) { currentTallest = $(this).outerHeight(); }
 		});
 		if (!px && Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
-		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'height': currentTallest}); }
 		$(this).children().css({'min-height': currentTallest}); 
 	});
 	return this;
@@ -39,8 +37,6 @@ $.fn.equalWidths = function(px) {
 				if($(this).outerWidth() > currentWidest) { currentWidest = $(this).outerWidth(); }
 		});
 		if(!px && Number.prototype.pxToEm) currentWidest = currentWidest.pxToEm(); //use ems unless px is specified
-		// for ie6, set width since min-width isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'width': currentWidest}); }
 		$(this).children().css({'min-width': currentWidest}); 
 	});
 	return this;
